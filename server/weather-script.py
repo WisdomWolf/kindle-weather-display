9041e00ccdb11f8abe55343fd81f3ed525a1b200
WisdomWolf <WisdomWolf@gmail.com>
2015-09-23 19:07:19 -0400

    Converted to functions and added shell commands directly to script

--- server/weather-script.py
+++ server/weather-script.py
@@ -15,63 +15,68 @@ except ImportError:
     # Python 2
     from urllib2 import urlopen
 
-#
-# Geographic location
-#
 
-latitude = 39.3286
-longitude = -76.6169
+def fetch_weather_data(latitude, longitude):
+    weather_xml = urlopen('http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=' + str(latitude) + '&lon=' + str(longitude) + '&format=24+hourly&numDays=4&Unit=e').read()
+    return minidom.parseString(weather_xml)
 
+def parse_temperatures(dom):
+    xml_temperatures = dom.getElementsByTagName('temperature')
+    highs = [None]*4
+    lows = [None]*4
+    for item in xml_temperatures:
+        if item.getAttribute('type') == 'maximum':
+            values = item.getElementsByTagName('value')
+            for i in range(len(values)):
+                highs[i] = int(values[i].firstChild.nodeValue)
+        if item.getAttribute('type') == 'minimum':
+            values = item.getElementsByTagName('value')
+            for i in range(len(values)):
+                lows[i] = int(values[i].firstChild.nodeValue)
+                
+    return highs,lows
 
+def parse_icons(dom):
+    xml_icons = dom.getElementsByTagName('icon-link')
+    icons = [None]*4
+    for i in range(len(xml_icons)):
+        icons[i] = xml_icons[i].firstChild.nodeValue.split('/')[-1].split('.')[0].rstrip('0123456789')
+        
+    return icons
 
-#
-# Download and parse weather data
-#
+def parse_dates(dom):
+    xml_day_one = dom.getElementsByTagName('start-valid-time')[0].firstChild.nodeValue[0:10]
+    return datetime.datetime.strptime(xml_day_one, '%Y-%m-%d')
 
-# Fetch data (change lat and lon to desired location)
-weather_xml = urlopen('http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=' + str(latitude) + '&lon=' + str(longitude) + '&format=24+hourly&numDays=4&Unit=e').read()
-dom = minidom.parseString(weather_xml)
+def process_svg(highs, lows, icons):
 
-# Parse temperatures
-xml_temperatures = dom.getElementsByTagName('temperature')
-highs = [None]*4
-lows = [None]*4
-for item in xml_temperatures:
-    if item.getAttribute('type') == 'maximum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            highs[i] = int(values[i].firstChild.nodeValue)
-    if item.getAttribute('type') == 'minimum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            lows[i] = int(values[i].firstChild.nodeValue)
+    # Open SVG to process
+    output = codecs.open('weather-script-base.svg', 'r', encoding='utf-8').read()
 
-# Parse icons
-xml_icons = dom.getElementsByTagName('icon-link')
-icons = [None]*4
-for i in range(len(xml_icons)):
-    icons[i] = xml_icons[i].firstChild.nodeValue.split('/')[-1].split('.')[0].rstrip('0123456789')
+    # Insert icons and temperatures
+    output = output.replace('ICON_TWO',icons[0])
+    output = output.replace('HIGH',str(highs[0])).replace('PI_TEMP','74').replace('REMOTE_TEMP','68')
+    output = output.replace('LOW',str(lows[0])).replace('REMOTE_HUM','100')
 
-# Parse dates
-xml_day_one = dom.getElementsByTagName('start-valid-time')[0].firstChild.nodeValue[0:10]
-day_one = datetime.datetime.strptime(xml_day_one, '%Y-%m-%d')
+    # Set Clock
+    output = output.replace('CLOCK',time.strftime('%I:%M %p', time.localtime()).lstrip('0'))
 
+    # Write output
+    codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
+    
+def run_cmd(cmd):
+    process = Popen(cmd.split(), stdout=PIPE)
+    output = process.communicate()[0]
+    return output
 
+LATITUDE = 39.3286
+LONGITUDE = -76.6169
 
-#
-# Preprocess SVG
-#
+weather_data = fetch_weather_data(LATITUDE, LONGITUDE)
+highs,lows = parse_temperatures(weather_data)
+icons = parse_icons(weather_data)
+day_one = parse_dates(weather_data)
+process_svg(highs, lows, icons)
 
-# Open SVG to process
-output = codecs.open('weather-script-base.svg', 'r', encoding='utf-8').read()
-
-# Insert icons and temperatures
-output = output.replace('ICON_TWO',icons[0])
-output = output.replace('HIGH',str(highs[0])).replace('PI_TEMP','74').replace('REMOTE_TEMP','68')
-output = output.replace('LOW',str(lows[0])).replace('REMOTE_HUM','100')
-
-# Set Clock
-output = output.replace('CLOCK',time.strftime('%I:%M %p', time.localtime()).lstrip('0'))
-
-# Write output
-codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
+run_cmd("rsvg-convert --background-color=white -o weather-script.png weather-script-output.svg")
+run_cmd("pngcrush -c 0 -ow weather-script.png weather-script-output.png")
