25cf20152da5752cf7081fb225f5f560c3bbcac4
WisdomWolf <wisdomwolf@gmail.com>
2015-09-23 18:46:47 -0400

    altered script to handle local temperatures and display clock

--- server/weather-script.py
+++ server/weather-script.py
@@ -6,6 +6,7 @@
 
 from xml.dom import minidom
 import datetime
+import time
 import codecs
 try:
     # Python 3
@@ -18,8 +19,8 @@ except ImportError:
 # Geographic location
 #
 
-latitude = 35.1975
-longitude = -80.8345
+latitude = 39.3286
+longitude = -76.6169
 
 
 
@@ -62,17 +63,15 @@ day_one = datetime.datetime.strptime(xml_day_one, '%Y-%m-%d')
 #
 
 # Open SVG to process
-output = codecs.open('weather-script-preprocess_pw.svg', 'r', encoding='utf-8').read()
+output = codecs.open('weather-script-base.svg', 'r', encoding='utf-8').read()
 
 # Insert icons and temperatures
-output = output.replace('ICON_ONE',icons[0]).replace('ICON_TWO',icons[1]).replace('ICON_THREE',icons[2]).replace('ICON_FOUR',icons[3])
-output = output.replace('HIGH_ONE',str(highs[0])).replace('HIGH_TWO',str(highs[1])).replace('HIGH_THREE',str(highs[2])).replace('HIGH_FOUR',str(highs[3]))
-output = output.replace('LOW_ONE',str(lows[0])).replace('LOW_TWO',str(lows[1])).replace('LOW_THREE',str(lows[2])).replace('LOW_FOUR',str(lows[3]))
+output = output.replace('ICON_TWO',icons[0])
+output = output.replace('HIGH',str(highs[0])).replace('PI_TEMP','74').replace('REMOTE_TEMP','68')
+output = output.replace('LOW',str(lows[0])).replace('REMOTE_HUM','100')
 
-# Insert days of week
-one_day = datetime.timedelta(days=1)
-days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-output = output.replace('DAY_THREE',days_of_week[(day_one + 2*one_day).weekday()]).replace('DAY_FOUR',days_of_week[(day_one + 3*one_day).weekday()])
+# Set Clock
+output = output.replace('CLOCK',time.strftime('%I:%M %p', time.localtime()).lstrip('0'))
 
 # Write output
 codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
