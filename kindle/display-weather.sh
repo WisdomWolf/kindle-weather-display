28d33247b4fef3228b0a72f4d4b950b3b6208b07
WisdomWolf <wisdomwolf@gmail.com>
2015-09-26 16:56:06 -0400

    adjusted kindle scripts for paperwhite

--- kindle/display-weather.sh
+++ kindle/display-weather.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-
-rm weather-script-output.png
-eips -c
-eips -c
-
-if wget https://www.dropbox.com/s/s4t1h716sbntkea/weather-script-output.png; then
-	eips -g weather-script-output.png
-else
-	eips -g weather-image-error.png
-fi
+python /mnt/us/weather/kindle_screen_update.py
