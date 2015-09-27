7ac4d70725fe03315eeddce10505be5405ee20c5
WisdomWolf <WisdomWolf@gmail.com>
2015-09-26 22:58:30 -0400

    updated local kindle scripts to pull in updates

--- kindle/kindle_screen_update.py
+++ kindle/kindle_screen_update.py
@@ -8,12 +8,18 @@ import os
 last_png = 0
 retries = 0
 
+# starting redraws above threshold to ensure full refresh at start
+MAX_REDRAWS = 6
+redraws = MAX_REDRAWS + 1
+
+
 def run_cmd(cmd):
     process = Popen(cmd.split(), stdout=PIPE)
     output = process.communicate()[0]
     return output
     
 def wait_for_next_minute(current=time.strftime('%M', time.localtime())):
+    check_for_update()
     while True:
         if time.strftime('%M', time.localtime()) == current:
             continue
@@ -30,6 +36,7 @@ def efficient_time_update():
 def check_for_update():
     global last_png
     global retries
+    run_cmd("wget -q http://192.168.1.43/images/weather-script-output.png -O weather.png")
     output = os.stat('weather.png').st_mtime
     if output == last_png and retries < 10:
         retries += 1
@@ -44,10 +51,17 @@ def check_for_update():
         os._exit(0)
 
 def update_screen():
-    print("updating screen at {0}".format(time.strftime("%c", time.localtime())))
-    run_cmd("eips -c")
-    run_cmd("eips -c")
-    run_cmd("eips -fg /mnt/us/weather/weather.png")
+    global redraws
+    if redraws <= MAX_REDRAWS:
+        print("updating screen at {0}".format(time.strftime("%c", time.localtime())))
+        run_cmd("eips -g /mnt/us/weather/weather.png")
+        redraws += 1
+    else:
+        print("refreshing screen at {0}".format(time.strftime("%c", time.localtime())))
+        run_cmd("eips -c")
+        run_cmd("eips -c")
+        run_cmd("eips -fg /mnt/us/weather/weather.png")
+        redraws = 0
     
 print("Script started at {0}".format(time.strftime("%c", time.localtime())))
 wait_for_next_minute()
